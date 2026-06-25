1462aff5fdb16adb257da58dc9b638d29e758c88
Riya Banik <82664305+riya89@users.noreply.github.com>
2026-06-25 18:18:53 +0530

    Time: 0 ms (100%), Space: 13 MB (15.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int findNext(vector<int>& nums2, int x){
-        int index =-1;
-        int n = nums2.size();
-        for(int i =0; i<n; i++){
-            if(nums2[i]==x){
-                index = i;
-                break;
-            }
-        }
-        for(int j=index+1; j<n; j++){
-            if(nums2[j]>x) return nums2[j];
-        }
-        return -1;
-    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         map<int, int> mpp;
+        stack<int> st;
         vector<int> ans(nums1.size(), -1);
         for(int i=0; i<nums1.size(); i++){
             mpp[nums1[i]]=i;
         }
-        for(int i=0; i<nums2.size(); i++){
-            if(mpp.find(nums2[i])!=mpp.end()){
-                ans[mpp[nums2[i]]]=findNext(nums2, nums2[i]);
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
             }
+            if(mpp.find(nums2[i])!=mpp.end()){
+                if(st.empty()) ans[mpp[nums2[i]]]=-1;
+                else ans[mpp[nums2[i]]]=st.top();
+            }
+            st.push(nums2[i]);
         }
         return ans;
     }
