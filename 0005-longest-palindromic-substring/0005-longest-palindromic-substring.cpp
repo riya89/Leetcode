03cea829d6282935e65f27a5911bb25aedbdba12
Riya Banik <82664305+riya89@users.noreply.github.com>
2026-06-29 12:50:41 +0530

    Time: 856 ms (7.69%), Space: 9.4 MB (79.77%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool solve(string &s, int i , int j){
+        if(i>=j) return true;
+        if(s[i]==s[j]) return solve(s, i+1, j-1);
+        else
+        return false;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int start =0;
+        int maxi = INT_MIN;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(solve(s, i, j)==true){
+                    if(j-i+1>maxi){
+                        maxi = j-i+1;
+                        start=i;
+                    }
+                }
+            }
+        }
+        return s.substr(start, maxi);
+    }
+};
