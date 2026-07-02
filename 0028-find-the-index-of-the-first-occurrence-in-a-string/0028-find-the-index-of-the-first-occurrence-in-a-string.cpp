e797e13326cf32c516f117b255e9b71504e2ede1
Riya Banik <82664305+riya89@users.noreply.github.com>
2026-07-02 16:32:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (6.4%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,15 +3,25 @@ public:
     int strStr(string haystack, string needle) {
         int n = haystack.size();
         int m = needle.size();
-        for(int i=0; i<=n-m; i++){
-            int j =0;
-            while(j<m && haystack[i+j]==needle[j]){
-                //we dont want to modify i so (i+j)
-                j++;
+        string res = needle + '$' + haystack;
+        vector<int> z(n+m+1,0);
+        int l=0; int r=0;
+        for(int i=1; i<n+m+1; i++){
+            //inside the box
+            if(i<=r){
+                z[i]=min(z[i-l], r-i+1);
             }
-            if(j==m){
-                return i;
+            //extend
+            while(i+z[i]<res.size() && res[z[i]]==res[i+z[i]]){
+                z[i]++;
             }
+            if(i+z[i]-1>r){
+                l=i;
+                r=i+z[i]-1;
+            }
+        }
+        for(int i=m+1; i<n+m+1; i++){
+            if(z[i]==m) return (i-m-1);
         }
         return -1;
     }
