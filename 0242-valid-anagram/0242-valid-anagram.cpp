3105b1a165f6bfa71ef217ded66ee0982071808b
Riya Banik <82664305+riya89@users.noreply.github.com>
2026-07-03 22:24:40 +0530

    Time: 3 ms (46.37%), Space: 9.8 MB (25.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -12,6 +12,8 @@ public:
         for(int i=0; i<26; i++){
             if(freq[i]>0) return false;
         }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
